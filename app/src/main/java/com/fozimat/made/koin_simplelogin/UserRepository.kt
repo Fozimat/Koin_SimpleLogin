c7db59c326a2e7f7c93ea7dba8aeebb72250f01d
Fozimat <fozimata@gmail.com>
2021-05-25 20:26:41 +0700

    Implementation Koin Testing

--- app/src/main/java/com/fozimat/made/koin_simplelogin/UserRepository.kt
+++ app/src/main/java/com/fozimat/made/koin_simplelogin/UserRepository.kt
@@ -1,15 +1,6 @@
 package com.fozimat.made.koin_simplelogin
 
 class UserRepository(private val sesi: SessionManager) {
-    companion object {
-        @Volatile
-        private var instance: UserRepository? = null
-
-        fun getInstance(sesi: SessionManager): UserRepository =
-            instance ?: synchronized(this) {
-                instance ?: UserRepository(sesi)
-            }
-    }
 
     fun loginUser(username: String) {
         sesi.createLoginSession()
