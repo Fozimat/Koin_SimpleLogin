b4b6d60dabbbba2ba142e23c9e18b08c3277a6b3
Fozimat <fozimata@gmail.com>
2021-05-25 19:22:38 +0700

    Initial commit

---
+++ app/src/main/java/com/fozimat/made/koin_simplelogin/HomeActivity.kt
@@ -0,0 +1,34 @@
+package com.fozimat.made.koin_simplelogin
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fozimat.made.koin_simplelogin.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var userRepository: UserRepository
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sesi = SessionManager(this)
+        userRepository = UserRepository.getInstance(sesi)
+
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
+
+        binding.btnLogout.setOnClickListener {
+            userRepository.logoutUser()
+            moveToMainActivity()
+        }
+    }
+
+    private fun moveToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
